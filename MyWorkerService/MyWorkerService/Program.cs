623a7420ced332ff7e9a0afa4a8d5fa75e2c06f8
Carlos Caceres <carloscc@cainco.org.bo>
2024-10-07 11:31:29 -0400

    Agregado Proyecto con plantilla Worker

---
+++ MyWorkerService/MyWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using MyWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
